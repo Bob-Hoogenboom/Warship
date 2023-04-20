035babbd3def38ac68f404e64e85948614e1bd52
Diego Ramon <29244@ma-web.nl>
2023-04-20 13:00:33 +0200

    Temporary Border

---
+++ WarshipGame/Assets/Scripts/Gameplay/AttackInformation.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackInformation : MonoBehaviour
+{
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Image BoatProfile;
+
+    void OnShipSelected()
+    {
+        
+    }
+}
