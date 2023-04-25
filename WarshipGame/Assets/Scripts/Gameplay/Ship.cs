6534c8b05bc7210b1e41bfae99fd5174ddb43b03
Diego Ramon <29244@ma-web.nl>
2023-04-25 11:15:43 +0200

    Image fix + art implementation

--- WarshipGame/Assets/Scripts/Gameplay/Ship.cs
+++ WarshipGame/Assets/Scripts/Gameplay/Ship.cs
@@ -33,7 +33,7 @@ public class Ship : MonoBehaviour
     public int MovementPoints => points;
     public event Action<Ship> MovementFinished;
     public bool shipMoved;
-    public int profileTag;
+    public Sprite profileTag;
     /// <summary>
     /// Gets glow manager to prevent Editor usage
     /// </summary>
