8092cf7a12474d36c6a1cbfc77ca44d01bc2a953
Bob Hoogenboom <28602@ma-web.nl>
2023-05-09 17:14:32 +0200

    camera panning working
    you can move the camera just like in civilization. only needs a panning speed and WASD as a 2nd option for moving the cam

--- WarshipGame/Assets/Scripts/Gameplay/Camera/CameraController.cs
+++ WarshipGame/Assets/Scripts/Gameplay/Camera/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Cinemachine;
 using Unity.VisualScripting;
@@ -5,21 +6,27 @@ using Unity.VisualScripting;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera CMVirtualCamera;
-    [SerializeField] private Camera Cam;
+    [SerializeField] private Camera Cam;     
+    [SerializeField]private Transform _dollyTransform;
 
     [Header("Panning")]
     [SerializeField] private Vector2 MapSize;
+    [SerializeField] private float panningSpeed;
+    private Vector3 _cameraOrigin;
     private Vector3 _dragOrigin;
+    private Vector3 _dragDirection;
+    
     
     [Header("Zooming")]
+    [Tooltip("The FOV of the camera will change to this value once a focus point is selected.")]
     [SerializeField] private float FocusValue;
     [SerializeField] private float ZoomValue;
     [SerializeField] private float ZoomSpeed;
 
     private GameObject _focusGameObject;
     private float _focusBoundaries;
-    
-    private void Update()
+
+    private void FixedUpdate()
     {
         PanCamera();
         ZoomCamera();
@@ -31,19 +38,34 @@ public class CameraController : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            _dragOrigin = Cam.ScreenToWorldPoint(Input.mousePosition);
-            //Debug.Log(Input.mousePosition);
-            //var testVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Cam.nearClipPlane);
-            //Debug.Log(testVec);
-            print(_dragOrigin);
+            _dragOrigin = Input.mousePosition;
+            _cameraOrigin = CMVirtualCamera.transform.position;
         }
 
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 difference = _dragOrigin - Cam.ScreenToWorldPoint(Input.mousePosition);
+        if (!Input.GetMouseButton(0)) return;
+        if (Input.GetAxis("Mouse X") == 0 && Input.GetAxis("Mouse Y") == 0) return;
+        
+        _dragDirection = Input.mousePosition;
+        Vector3 screenDelta = _dragOrigin - _dragDirection;
+        
+        //change Cam for cinemachine variables?
+        Vector2 screenSize = ScaleScreenToWorldSize(Cam.aspect, Cam.orthographicSize, Cam.scaledPixelWidth, Cam.scaledPixelHeight, screenDelta.x, screenDelta.y);
+        
+        Vector3 move = new Vector3(screenSize.x + screenSize.y, 0f, screenSize.y - screenSize.x);
+        Vector3 camPosMove = new Vector3(_cameraOrigin.x - move.x, _cameraOrigin.y, _cameraOrigin.z - move.z);
+        
+        CMVirtualCamera.transform.position =  camPosMove ;
+    }
 
-            CMVirtualCamera.transform.position += difference;
-        }
+    private Vector2 ScaleScreenToWorldSize(float camAspect, float camFOV, float camScreenPixelWidth,
+        float camScreenPixelHeight, float screenW, float screenH)
+    {
+        float cameraWidth = camAspect * camFOV * 2f;
+        float cameraHeight = camFOV * 2f;
+        float screenWorldW = screenW * (cameraWidth / camScreenPixelWidth);
+        float screenWorldH = screenH * (cameraHeight / camScreenPixelHeight);
+        
+        return new Vector2(screenWorldW, screenWorldH);
     }
 
     private void ZoomCamera()
