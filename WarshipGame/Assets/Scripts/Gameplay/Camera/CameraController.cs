b69dcb148e55f9a9c7c245d8f2f89338e9a10915
Bob Hoogenboom <28602@ma-web.nl>
2023-05-16 10:12:56 +0200

    Update CameraController.cs
    Changed pull-request comments

--- WarshipGame/Assets/Scripts/Gameplay/Camera/CameraController.cs
+++ WarshipGame/Assets/Scripts/Gameplay/Camera/CameraController.cs
@@ -4,17 +4,17 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera CMVirtualCamera;
-    [SerializeField] private Camera Cam;
+    [SerializeField] private CinemachineVirtualCamera cmVirtualCamera;
+    [SerializeField] private Camera cam;
 
     [Header("Panning")]
     
     [Tooltip("The speed in which the player can drag the camera across the game scene.")]
-    [SerializeField] private float PanningSpeed = 10f;
+    [SerializeField] private float panningSpeed = 10f;
     [Tooltip("The amount of Hexes in the X-axis on the map.")]
-    [SerializeField] private float MapSizeX = 8f;
+    [SerializeField] private float mapSizeX = 8f;
     [Tooltip("The amount of Hexes in the Z-axis on the map.")]
-    [SerializeField] private float MapSizeZ = 8f;
+    [SerializeField] private float mapSizeZ = 8f;
     private Vector3 _cameraOrigin;
     private Vector3 _dragOrigin;
     private Vector3 _dragDirection;
@@ -22,36 +22,42 @@ public class CameraController : MonoBehaviour
     [Header("Zooming")]
     
     [Tooltip("The FOV of the camera will change to this value once a focus point is selected.")]
-    [SerializeField] private float FocusValue = 30f;
+    [SerializeField] private float focusValue = 30f;
     private GameObject _focusGameObject;
     private float _focusBoundaries;
     private bool _isFocussing;
     
     [Space]
     [Tooltip("The Cameras FOV will always be in between these 2 values.")]
-    [SerializeField] private Vector2 MaxZoom = new(35f,60f);
+    [SerializeField] private Vector2 maxZoom = new(35f,60f);
     [Tooltip("The Speed in which the player can zoom the camera FOV.")]
-    [SerializeField] private float ZoomSpeed = 9;
+    [SerializeField] private float zoomSpeed = 9;
     [Tooltip("The standard zoomFOV for this camera controller.")] 
-    [SerializeField] private float DefaultFOVValue = 45;
+    [SerializeField] private float defaultFOVValue = 45;
     private float _currentFOVValue;
     
+    /// <summary>
+    /// Awake first sets the size of the map by calculating mapSizeX and mapSizeY
+    /// with the hexDiameter.
+    /// And the Default FOV is set to the camera
+    /// </summary>
     private void Awake()
     {
-        MapSizeX *= 0.866f;
-        MapSizeZ = MapSizeZ * 0.866f - (0.866f / 2);
+        mapSizeX *= 0.866f;
+        mapSizeZ = mapSizeZ * 0.866f - (0.866f / 2);
         
-        _currentFOVValue = DefaultFOVValue;
-        CMVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
+        _currentFOVValue = defaultFOVValue;
+        cmVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
     }
 
     private void FixedUpdate()
     {
         PanCamera();
 
-        if (Input.GetAxis("Mouse ScrollWheel") != 0 && !_isFocussing)
+        float localInput = Input.GetAxis("Mouse ScrollWheel");
+        if (localInput != 0 && !_isFocussing)
         {
-            ZoomCamera();
+            ZoomCamera(localInput);
         }
     }
 
@@ -64,7 +70,7 @@ public class CameraController : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             _dragOrigin = Input.mousePosition;
-            _cameraOrigin = CMVirtualCamera.transform.position;
+            _cameraOrigin = cmVirtualCamera.transform.position;
         }
 
         if (!Input.GetMouseButton(0)) return;
@@ -73,18 +79,18 @@ public class CameraController : MonoBehaviour
         _dragDirection = Input.mousePosition;
         Vector3 screenDelta = _dragOrigin - _dragDirection;
 
-        Vector2 screenSize = ScaleScreenToWorldSize(Cam.aspect, Cam.orthographicSize, Cam.scaledPixelWidth, Cam.scaledPixelHeight, screenDelta.x, screenDelta.y);
+        Vector2 screenSize = ScaleScreenToWorldSize(cam.aspect, cam.orthographicSize, cam.scaledPixelWidth, cam.scaledPixelHeight, screenDelta.x, screenDelta.y);
 
-        Vector3 move = new Vector3(screenSize.x + screenSize.y, 0f, screenSize.y - screenSize.x);
+        Vector3 move = new (screenSize.x + screenSize.y, 0f, screenSize.y - screenSize.x);
         move = Quaternion.Euler(0,-90,0) * move;
-        Vector3 camPosMove = new Vector3(_cameraOrigin.x + move.x * Time.deltaTime * PanningSpeed, _cameraOrigin.y, _cameraOrigin.z + move.z * Time.deltaTime * PanningSpeed);
+        Vector3 camPosMove = new (_cameraOrigin.x + move.x * Time.deltaTime * panningSpeed, _cameraOrigin.y, _cameraOrigin.z + move.z * Time.deltaTime * panningSpeed);
 
-        if (camPosMove.z <= -MapSizeZ || camPosMove.z >= MapSizeZ || camPosMove.x <= -MapSizeX || camPosMove.x >= MapSizeX)
+        if (camPosMove.z <= -mapSizeZ || camPosMove.z >= mapSizeZ || camPosMove.x <= -mapSizeX || camPosMove.x >= mapSizeX)
         {  
             return;
         }
         
-        CMVirtualCamera.transform.position =  camPosMove;
+        cmVirtualCamera.transform.position =  camPosMove;
     }
 
     /// <summary>
@@ -97,36 +103,35 @@ public class CameraController : MonoBehaviour
     {
         float cameraWidth = camAspect * camFOV * 2f;
         float cameraHeight = camFOV * 2f;
-        float screenWorldW = screenW * (cameraWidth / camScreenPixelWidth);
-        float screenWorldH = screenH * (cameraHeight / camScreenPixelHeight);
+        float screenWorldWidth = screenW * (cameraWidth / camScreenPixelWidth);
+        float screenWorldHeight = screenH * (cameraHeight / camScreenPixelHeight);
         
-        return new Vector2(screenWorldW, screenWorldH);
+        return new Vector2(screenWorldWidth, screenWorldHeight);
     }
 
     /// <summary>
     /// Zooms the camera when the scrolling axis is changed
     /// </summary>
-    private void ZoomCamera()
+    private void ZoomCamera(float scrollValue)
     {
-        float scrollValue = Input.GetAxis("Mouse ScrollWheel");
-        float newZoomValue = _currentFOVValue -= scrollValue * ZoomSpeed;
+        float newZoomValue = _currentFOVValue - scrollValue * zoomSpeed;
 
-        if (newZoomValue <= MaxZoom.x)
+        if (newZoomValue <= maxZoom.x)
         {
-            _currentFOVValue = MaxZoom.x;
-            CMVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
+            _currentFOVValue = maxZoom.x;
+            cmVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
             return;
         }
         
-        if (newZoomValue >= MaxZoom.y)
+        if (newZoomValue >= maxZoom.y)
         {
-            _currentFOVValue = MaxZoom.y;
-            CMVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
+            _currentFOVValue = maxZoom.y;
+            cmVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
             return;
         }
         
         _currentFOVValue = newZoomValue;
-        CMVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
+        cmVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
     }
 
     /// <summary>
@@ -144,12 +149,12 @@ public class CameraController : MonoBehaviour
         Vector3 rayHitPosition = hit.point;
         
         
-        Vector2 selectedShipPosition = new Vector2(resultPosition.x, resultPosition.z);
-        Vector2 hitPosition = new Vector2(rayHitPosition.x, rayHitPosition.z);
+        Vector2 selectedShipPosition = new (resultPosition.x, resultPosition.z);
+        Vector2 hitPosition = new (rayHitPosition.x, rayHitPosition.z);
         
         Vector2 difference = hitPosition - selectedShipPosition;
 
-        Vector3 newPosition = new Vector3(vCamPosition.x - difference.x, vCamPosition.y, vCamPosition.z - difference.y);
+        Vector3 newPosition = new (vCamPosition.x - difference.x, vCamPosition.y, vCamPosition.z - difference.y);
         transform.position = newPosition;
         
         StartCoroutine(IsInFocus(newPosition));
@@ -161,15 +166,15 @@ public class CameraController : MonoBehaviour
     /// </summary>
     /// <param name="focusPosition"></param>
     /// <returns></returns>
-    IEnumerator IsInFocus(Vector3 focusPosition)
+    private IEnumerator IsInFocus(Vector3 focusPosition)
     {
         while (Vector3.Distance(transform.position, focusPosition) <= 1f) 
         {
-            CMVirtualCamera.m_Lens.FieldOfView = FocusValue;
+            cmVirtualCamera.m_Lens.FieldOfView = focusValue;
             yield return null;
         }
     
-        CMVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
+        cmVirtualCamera.m_Lens.FieldOfView = _currentFOVValue;
         _isFocussing = false;
     }
 }
