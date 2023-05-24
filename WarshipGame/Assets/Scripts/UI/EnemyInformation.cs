41d7d6315b67aaa973561f42a83aba1290eb89aa
Diego Ramon <29244@ma-web.nl>
2023-05-24 12:47:19 +0200

    Enemy border informatie finished + bugs fixed

--- WarshipGame/Assets/Scripts/UI/EnemyInformation.cs
+++ WarshipGame/Assets/Scripts/UI/EnemyInformation.cs
@@ -1,22 +1,23 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyInformation : MonoBehaviour
 { 
     [SerializeField] private Ship _enemyShip;
+    [SerializeField] private Image enemyBorder;
+    [SerializeField] private Sprite deadShipBorder;
     private Image enemyPictures;
 
     private void Awake()
     {
         enemyPictures = GetComponent<Image>();
-        enemyPictures.sprite = _enemyShip.GetComponent<Ship>().profileTag;;
+        enemyPictures.sprite = _enemyShip.GetComponent<Ship>().profileTag;
     }
 
     private void Update()
     {
-        
+        if (!_enemyShip.shipDead) return;
+        enemyBorder.sprite = deadShipBorder;
     }
 }
 
